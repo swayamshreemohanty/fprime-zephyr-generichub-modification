c982063e2c015e1df5dea3ede7283b9307a6467c
Swayamshree Mohanty <swayamshree.mohanty@serendipityspace.in>
2026-06-27 19:38:40 +0530

    enabled the rts cts

--- fprime-zephyr/Drv/ZephyrUartDriver/ZephyrUartDriver.cpp
+++ fprime-zephyr/Drv/ZephyrUartDriver/ZephyrUartDriver.cpp
@@ -4,117 +4,96 @@
 // \brief  cpp file for ZephyrUartDriver component implementation class
 // ======================================================================
 
-
 #include "fprime-zephyr/Drv/ZephyrUartDriver/ZephyrUartDriver.hpp"
-#include "Fw/Types/BasicTypes.hpp"
-#include "Fw/Types/Assert.hpp"
 #include <Fw/FPrimeBasicTypes.hpp>
+#include "Fw/Types/Assert.hpp"
+#include "Fw/Types/BasicTypes.hpp"
 
 namespace Zephyr {
 
-    // ----------------------------------------------------------------------
-    // Construction, initialization, and destruction
-    // ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// Construction, initialization, and destruction
+// ----------------------------------------------------------------------
 
-    ZephyrUartDriver ::
-        ZephyrUartDriver(
-            const char *const compName
-        ) : ZephyrUartDriverComponentBase(compName)
-    {
+ZephyrUartDriver ::ZephyrUartDriver(const char* const compName) : ZephyrUartDriverComponentBase(compName) {}
+
+ZephyrUartDriver ::~ZephyrUartDriver() {}
+
+void ZephyrUartDriver::configure(const struct device* dev, U32 baud_rate) {
+    FW_ASSERT(dev != nullptr);
+    m_dev = dev;
+
+    if (!device_is_ready(this->m_dev)) {
+        return;
     }
 
-    ZephyrUartDriver ::
-        ~ZephyrUartDriver()
-    {
+    struct uart_config uart_cfg;
+    int err = uart_config_get(this->m_dev, &uart_cfg);
+    if (err == 0) {
+        // Keep device tree defaults (including hw-flow-control)
+        uart_cfg.baudrate = baud_rate;
+    } else {
+        // Fallback if config_get is unsupported
+        uart_cfg.baudrate = baud_rate;
+        uart_cfg.parity = UART_CFG_PARITY_NONE;
+        uart_cfg.stop_bits = UART_CFG_STOP_BITS_1;
+        uart_cfg.data_bits = UART_CFG_DATA_BITS_8;
+        uart_cfg.flow_ctrl = UART_CFG_FLOW_CTRL_NONE;
+    }
+    uart_configure(this->m_dev, &uart_cfg);
 
+    ring_buf_init(&this->m_ring_buf, RING_BUF_SIZE, this->m_ring_buf_data);
+
+    uart_irq_callback_user_data_set(this->m_dev, serial_cb, this);
+    uart_irq_rx_enable(this->m_dev);
+
+    if (this->isConnected_ready_OutputPort(0)) {
+        this->ready_out(0);
+    }
+}
+
+void ZephyrUartDriver::serial_cb(const struct device* dev, void* user_data) {
+    ZephyrUartDriver* driver = static_cast<ZephyrUartDriver*>(user_data);
+
+    if (!uart_irq_update(dev)) {
+        return;
     }
 
-    void ZephyrUartDriver::configure(const struct device *dev, U32 baud_rate) {
-        FW_ASSERT(dev != nullptr);
-        m_dev = dev;
-
-        if (!device_is_ready(this->m_dev)) {
-            return;
-        }
-
-        struct uart_config uart_cfg = {
-            .baudrate = baud_rate,
-            .parity = UART_CFG_PARITY_NONE,
-            .stop_bits = UART_CFG_STOP_BITS_1,
-            .data_bits = UART_CFG_DATA_BITS_8,
-            .flow_ctrl = UART_CFG_FLOW_CTRL_NONE,
-        };
-        uart_configure(this->m_dev, &uart_cfg);
-
-        ring_buf_init(&this->m_ring_buf, RING_BUF_SIZE, this->m_ring_buf_data);
-        uart_irq_callback_user_data_set(this->m_dev, serial_cb, &this->m_ring_buf);
-
-        uart_irq_rx_enable(this->m_dev);
-	    uart_irq_tx_disable(this->m_dev);
-
-        if (this->isConnected_ready_OutputPort(0)) {
-            this->ready_out(0);
+    if (uart_irq_rx_ready(dev)) {
+        U8 buf[64];
+        int recv_len = uart_fifo_read(dev, buf, sizeof(buf));
+        if (recv_len > 0) {
+            ring_buf_put(&driver->m_ring_buf, buf, recv_len);
         }
     }
+}
 
-    void ZephyrUartDriver::serial_cb(const struct device *dev, void *user_data)
-    {
-        struct ring_buf *ring_buf = reinterpret_cast<struct ring_buf *>(user_data);
+// ----------------------------------------------------------------------
+// Handler implementations for user-defined typed input ports
+// ----------------------------------------------------------------------
 
-        if (!uart_irq_update(dev)) {
-            return;
-        }
+void ZephyrUartDriver ::schedIn_handler(const FwIndexType portNum, U32 context) {
+    Fw::Buffer recv_buffer = this->allocate_out(0, SERIAL_BUFFER_SIZE);
 
-        if (!uart_irq_rx_ready(dev)) {
-            return;
-        }
-
-        U8 c;
-        // TODO: Get rid of the endless loop (in an IRQ handler!).
-        while (uart_fifo_read(dev, &c, 1) == 1) {
-            if (ring_buf_put(ring_buf, &c, 1) != 1) {
-                // TODO: Handle properly.
-                printk("UART buffer overrun\n");
-            }
-        }
+    U32 recv_size = ring_buf_get(&this->m_ring_buf, recv_buffer.getData(), recv_buffer.getSize());
+    if (recv_size == 0) {
+        // No data received, deallocate buffer
+        this->deallocate_out(0, recv_buffer);
+    } else {
+        recv_buffer.setSize(recv_size);
+        recv_out(0, recv_buffer, Drv::ByteStreamStatus::OP_OK);
     }
+}
 
-    // ----------------------------------------------------------------------
-    // Handler implementations for user-defined typed input ports
-    // ----------------------------------------------------------------------
-
-    void ZephyrUartDriver ::
-        schedIn_handler(
-            const FwIndexType portNum,
-            U32 context
-        )
-    {
-        Fw::Buffer recv_buffer = this->allocate_out(0, SERIAL_BUFFER_SIZE);
-
-        U32 recv_size = ring_buf_get(&this->m_ring_buf, recv_buffer.getData(), recv_buffer.getSize());
-        if (recv_size == 0) {
-            // No data received, deallocate buffer
-            this->deallocate_out(0, recv_buffer);
-        } else {
-            recv_buffer.setSize(recv_size);
-            recv_out(0, recv_buffer, Drv::ByteStreamStatus::OP_OK);
-        }
+Drv::ByteStreamStatus ZephyrUartDriver ::send_handler(const FwIndexType portNum, Fw::Buffer& sendBuffer) {
+    for (U32 i = 0; i < sendBuffer.getSize(); i++) {
+        uart_poll_out(this->m_dev, sendBuffer.getData()[i]);
     }
+    return Drv::ByteStreamStatus::OP_OK;
+}
 
-    Drv::ByteStreamStatus ZephyrUartDriver ::
-        send_handler(
-            const FwIndexType portNum,
-            Fw::Buffer &sendBuffer
-        )
-    {
-        for (U32 i = 0; i < sendBuffer.getSize(); i++) {
-            uart_poll_out(this->m_dev, sendBuffer.getData()[i]);
-        }
-        return Drv::ByteStreamStatus::OP_OK;
-    }
+void ZephyrUartDriver ::recvReturnIn_handler(const FwIndexType portNum, Fw::Buffer& returnBuffer) {
+    this->deallocate_out(0, returnBuffer);
+}
 
-    void ZephyrUartDriver ::recvReturnIn_handler(const FwIndexType portNum, Fw::Buffer &returnBuffer) {
-        this->deallocate_out(0, returnBuffer);
-    }
-
-} // end namespace Zephyr
+}  // end namespace Zephyr
